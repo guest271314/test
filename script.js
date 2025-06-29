6a12291d3d86027927a0f2146e41ba494a52c990
guest271314 <guest271314@gmail.com>
2025-06-29 21:55:48 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+navigator.serviceWorker
+  .getRegistrations()
+  .then((r) => Promise.all(r.map((s) => s.unregister())))
+  .then(() =>
+    navigator.serviceWorker.register(`./sw.js?=${new Date.getTime()}`, {
+      scope: "./",
+      type: "module",
+    })
+  )
+  .then((s) => {
+    return new Promise((resolve) => {
+      navigator.serviceWorker.addEventListener(
+        "controllerchange",
+        (e) => {
+          console.log(e);
+          resolve(e);
+        },
+        { once: true },
+      );
+    });
+  })
+  .catch((e) => e.message);
+
+export {};
